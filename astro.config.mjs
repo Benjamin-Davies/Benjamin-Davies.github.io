43d9bb5efcbb1bfa9678d2bdb4cfca7bc9833f6b
Benjamin Davies <bentendavies@gmail.com>
2026-01-20 20:39:18 +1300

    refactor: use tailwind for some styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bend.nz',
   base: '/',
   trailingSlash: 'always',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
